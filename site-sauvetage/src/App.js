9878802de1311c8314982fa725bac52a05f91a77
SandroSpina698 <spina.sandro69@gmail.com>
2021-12-02 22:05:44 +0100

    dossiers components etc

--- site-sauvetage/src/App.js
+++ site-sauvetage/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <BrowserRouter>
         <Routes>
           <Route exact path = "/" element = {<Home/>}/>
