1e322370bd654071cd1bdf68f2aa2cb8f0e3d91a
SandroSpina698 <spina.sandro69@gmail.com>
2021-12-03 04:07:36 +0100

    easter_egg + recherche

--- site-sauvetage/src/App.js
+++ site-sauvetage/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <BrowserRouter>
+      <BrowserRouter history>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/login" element={<LoginForm />} />
